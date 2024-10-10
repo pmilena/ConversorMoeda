5407a74a05b9c4ef8908c4a2a7afb00b8a27622b
Milena Pereira <milena.psilva03@gmail.com>
2024-10-10 16:22:56 -0300

    feat: add conversion history display option for user

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {    public static void main(String[] args) throws IOException, InterruptedException {
 
     Conversor conversor = new Conversor();
+    List<String> historico = new ArrayList<>();
 
     var choose = 0;
 
@@ -29,7 +29,7 @@ public class Main {    public static void main(String[] args) throws IOException
                     11- Sair
                     """);
 
-        try{
+        try {
             System.out.println("Escolha uma opção válida: ");
             System.out.println("***************************************");
             choose = leitor.nextInt();
@@ -41,51 +41,90 @@ public class Main {    public static void main(String[] args) throws IOException
             switch (choose) {
                 case 1:
                     conversor.Converter("USD", "ARS", leitor.nextDouble());
+                    historico.add("USD =>> ARS");
                     choose = 0;
                     break;
                 case 2:
                     conversor.Converter("ARS", "USD", leitor.nextDouble());
+                    historico.add("ARS =>> USD");
                     choose = 0;
                     break;
                 case 3:
                     conversor.Converter("USD", "BOB", leitor.nextDouble());
+                    historico.add("USD =>> BOB");
                     choose = 0;
                     break;
                 case 4:
                     conversor.Converter("BOB", "USD", leitor.nextDouble());
+                    historico.add("BOB =>> USD");
                     choose = 0;
                     break;
                 case 5:
                     conversor.Converter("USD", "BRL", leitor.nextDouble());
+                    historico.add("USD =>> BRL");
                     choose = 0;
                     break;
                 case 6:
                     conversor.Converter("BRL", "USD", leitor.nextDouble());
+                    historico.add("BRL =>> USD");
                     choose = 0;
                     break;
                 case 7:
                     conversor.Converter("USD", "CLP", leitor.nextDouble());
+                    historico.add("USD =>> CLP");
                     choose = 0;
                     break;
                 case 8:
                     conversor.Converter("CLP", "USD", leitor.nextDouble());
+                    historico.add("CLP =>> USD");
                     choose = 0;
                     break;
                 case 9:
                     conversor.Converter("USD", "COP", leitor.nextDouble());
+                    historico.add("USD =>> COP");
                     choose = 0;
                     break;
                 case 10:
                     conversor.Converter("COP", "USD", leitor.nextDouble());
+                    historico.add("COP =>> USD");
                     choose = 0;
                     break;
                 case 11:
-                    System.out.println("A aplicação foi finalizada");
+                    var print = "";
+
+                    System.out.println("""
+                                Deseja consultar o Histórico de Pesquisa:
+                                S=>> SIM
+                                N ==> NÃO
+                                """);
+
+                    while(print.isEmpty()) {
+                        print = leitor.next();
+
+                        if (print.equalsIgnoreCase("S")) {
+                            System.out.println("*****************************");
+                            System.out.println("Histórico de Conversões:");
+                            System.out.println("*****************************");
+
+                            Collections.reverse(historico);
+
+                            for (String item : historico) {
+                                System.out.println(item);
+                            }
+
+                        } else if (print.equalsIgnoreCase("N")) {
+                            System.out.println("A aplicação foi finalizada");
+                        } else {
+                            System.out.println("Digite 'S' para SIM ou 'N' para NÃO.");
+                            print = "";
+                        }
+                    }
                     break;
 
                 default:
                     System.out.println("O valor digitado não é válido. Tente novamente.");
             }
+
         }catch (InputMismatchException e) {
             System.out.println("O valor digitado não corresponde a um número. Tente novamente");
         } leitor.nextLine();
--- src/Requests.java
+++ src/Requests.java
@@ -20,3 +20,4 @@ public class Requests {
         return response.body();
     }
 }
+
